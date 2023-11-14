f55cf755598d5be369ec60dfcea7467446623fc2
Roman <didoshakr@ukr.net>
2023-11-14 20:35:18 +0200

    w

--- components/table/TableFooter.js
+++ components/table/TableFooter.js
@@ -104,7 +104,7 @@ const TableFooter = ({
         </div>
       </div>
       <div className="text inline-flex -space-x-px ">
-        {/* <a
+        <a
           className="flex items-center justify-center rounded-l-lg border border-gray-300 bg-white px-3 py-1 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
           onClick={() => {
             if (page > 1) setPage(page - 1);
@@ -126,9 +126,9 @@ const TableFooter = ({
                 d="M13 5H1m0 0 4 4M1 5l4-4"
                 />
             </svg>
-            Previous
-            </a> */}
-        {/* {range.map((el, index) =>
+            {/* Previous */}
+            </a>
+        {range.map((el, index) =>
           range.length < 6 ||
           el == 1 ||
           el == Math.max(...range) ||
@@ -155,7 +155,7 @@ const TableFooter = ({
             //   "..."
             ""
           ),
-        )} */}
+        )}
         <a
           className="flex items-center justify-center rounded-r-lg border border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
           onClick={() => {
@@ -164,7 +164,7 @@ const TableFooter = ({
           }}
           title="Наступна"
         >
-          Next
+          {/* Next */}
           <svg
             className="ml-2 h-3.5 w-3.5"
             aria-hidden="true"
