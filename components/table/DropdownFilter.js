fc7f6df2bd4efc3a2fadec8c4a97976039722863
Roman <didoshakr@ukr.net>
2023-11-29 19:08:02 +0200

    2

--- components/table/DropdownFilter.js
+++ components/table/DropdownFilter.js
@@ -108,8 +108,11 @@ export default function DropdownFilter({
             <polyline points="9 10 4 15 9 20" />{" "}
             <path d="M20 4v7a4 4 0 0 1-4 4H4" />
           </svg>
-          <h1 className={`{styleTableText} font-bold uppercase text-red-500 `}>
+          {/* <h1 className={`{styleTableText} font-bold uppercase text-red-500 `}>
             Застосувати фільтри
+          </h1> */}
+          <h1 className={`{styleTableText} font-bold uppercase text-red-500 `}>
+            Фільтри
           </h1>
         </button>
 
