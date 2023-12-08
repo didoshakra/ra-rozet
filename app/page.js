aa5f5bc4b18d3dfc4b0969614d67dcb787e286ec
Roman <didoshakr@ukr.net>
2023-12-08 15:15:47 +0200

    1

--- app/page.js
+++ app/page.js
@@ -7,10 +7,10 @@ import Promotion from "@/app/(shop)/product/Promotion";
 
 export default function Home() {
   return (
-    <main className="mx-auto mt-5 min-h-screen w-screen px-1">
+    <main className="mx-auto min-h-screen w-screen px-1">
       {/* <LayoutWithSidebar /> */}
       <StHomePage />
-      {/* <OurProducts /> */}
+      <OurProducts />
       {/* <Promotion /> */}
     </main>
   );
