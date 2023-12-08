ea755f24103adbf3d53415252183673a98aa5b27
Roman <didoshakr@ukr.net>
2023-12-08 13:15:54 +0200

    2

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
       {/* <LayoutWithSidebar /> */}
       <StHomePage />
       <OurProducts />
-      <Promotion />
+      {/* <Promotion /> */}
     </main>
   );
 }
